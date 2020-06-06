66f74c61230f26f1ff4a778e456c6c8c2ecf8ea2
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-06 10:37:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,4 @@
-dwarves = [
-            Doc,
-            Dopey,
-            Bashful,
-            Grumpy,
-            Sneezy,
-            Sleepy,
-            Happy,
-          ]
+
 
 
 def roll_call_dwarves(dwarves)
@@ -28,8 +20,7 @@ end
 
 
 def find_the_cheese(potentially_cheesy_items)
-  cheeses = [gouda, 
-            cheddar,
+  cheeses = [g cheddar,
             camembert]
 
   potentially_cheesy_items.find do |maybe_cheese|
