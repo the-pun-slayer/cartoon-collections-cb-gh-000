09196f779d1f0de5acf734f642bd42629c2613d0
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-06 10:34:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,41 +20,12 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
-summon_captain_planet(planeteer_calls)
-
-# The planeteer_calls are supposed to be short, so we want to
-# find out if any of our calls have more than 4 characters.
-#
-# Use the any? method
-# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-any-3F
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| call.length > 4 }
+  planeteer_calls.all? { |call| call.length < 4 }
 end
 
-long_planeteer_calls(planeteer_calls)
 
-# Help Monterey Jack find the cheese!
-
-# The "find_the_cheese" method should look through an array of strings
-# and find the first string the matches one of the following
-# cheese strings: "cheddar", "gouda", or "camembert"
-#
-# Given an array that looks like: ["shoe", "rope", "gouda"],
-# the value "gouda" should be returned by "find_the_cheese".
-#
-# Given an array that looks like: ["cheddar", "rope", "pie"],
-# the value "cheddar" should be returned by "find_the_cheese".
-#
-# If the array of items contains no cheese, then "find_the_cheese"
-# should return nil.
-
-# Assume that all strings will be lower-case.
-
-# Use the find method
-# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-find
-
-potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
 def find_the_cheese(potentially_cheesy_items)
   cheeses = %w[gouda cheddar camembert]
@@ -64,4 +35,3 @@ def find_the_cheese(potentially_cheesy_items)
   end
 end
 
-find_the_cheese(potentially_cheesy_items)
