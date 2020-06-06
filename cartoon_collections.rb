b93559ae509739fcdc28ac490ff4f99086433ddb
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-06 10:34:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ end
 
 
 def find_the_cheese(potentially_cheesy_items)
-  cheeses = %w[gouda cheddar camembert]
+  cheeses = [gouda, cheddar, camembert]
 
   potentially_cheesy_items.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
